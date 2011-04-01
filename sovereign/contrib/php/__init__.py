36bdf1edcef0db61dcc53fb685f91c492c901230
Brantley Harris <deadwisdom@gmail.com>
2011-04-01 00:07:36 -0500

    PHP Debug info.

--- sovereign/contrib/php/__init__.py
+++ sovereign/contrib/php/__init__.py
@@ -23,6 +23,8 @@ class PhpService(service.ProcessService):
     ]
     
     def start(self):
+        self.logger.debug("php service starting...")
+        
         address = self.settings['address']
         args = self.settings['args']
         options = self.settings['options']
@@ -51,6 +53,8 @@ class PhpService(service.ProcessService):
         self._static.set_handler('php', self._handle)
         
         self._fastcgi = FastCGI(self.address)
+        
+        self.logger.debug("PHP service ready.")
     
     def route_msg(self, environ=None, start_response=None):
         if 'REQUEST_URI' not in environ:
@@ -63,6 +67,8 @@ class PhpService(service.ProcessService):
                 request_uri.extend(['?', environ['QUERY_STRING']])
             environ['REQUEST_URI'] = "".join(request_uri)
             
+        self.logger.debug("PHP Route: %s" % (environ['REQUEST_URI']))
+        
         path = environ['PATH_INFO']
         if '.php/' in path:
             path, info = path.split('.php/', 1)
@@ -88,5 +94,7 @@ class PhpService(service.ProcessService):
         if (environ['REQUEST_METHOD'] == 'POST' and not environ.get('CONTENT_TYPE')):
             _SERVER['CONTENT_TYPE'] = 'application/x-www-form-urlencoded'
         
+        self.logger.debug("PHP Handle:", environ['REQUEST_INFO'])
         response = self._fastcgi(_SERVER, start_response)
+        self.logger.debug("PHP response: %r" % response)
         return response
